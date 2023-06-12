494ab5ac492aff4ec170211ce9a9ededf966ec78
Lennon Araujo <lennon_hih@hotmail.com>
2023-06-12 20:20:34 -0300

    create my route, iniate using redux

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+// import { toast } from 'react-toastify';
 
+import MyRoute from './MyRoute';
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
 
 export default function Routes() {
+  // toast.success('OIiiiiii', {
+  //   toastId: 'teste',
+  //   theme: 'dark',
+  // });
+  // toast.error('OIiiiiii', {
+  //   toastId: 'teste1',
+  //   theme: 'dark',
+  // });
+
   return (
     <Switch>
-      <Route exact path="/" component={Login} />
-      <Route path="*" component={Page404} />
+      <MyRoute exact path="/" component={Login} />
+      <MyRoute path="*" component={Page404} />
     </Switch>
   );
 }
